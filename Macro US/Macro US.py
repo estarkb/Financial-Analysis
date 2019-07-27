98c9dd0a4cc6a51c7aa2de28f1b470b3ead93c57
Ewald Stark <43977693+estarkb@users.noreply.github.com>
2019-07-27 19:57:31 -0400

    Add files via upload

---
+++ Macro US/Macro US.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from datetime import datetime
+import pandas_datareader.data as web
+start = datetime(1987,1,1)
+
+#S&P
+start = datetime(1987,1,1)
+sp = web.get_data_yahoo(["^GSPC"], start).fillna(method="ffill")
+sp = sp["Close"]
+sp = sp.T
+sp = sp.groupby(pd.PeriodIndex(sp.columns, freq='Q'), axis=1).mean()
+sp = sp.T
+
+#Data
+data = pd.read_excel(r"data.xlsx", index_col="Fecha").dropna()
+percentiles = data.rank(pct=True)
+percentiles["Spreas 10Y 2Y"] = 1-percentiles["Spreas 10Y 2Y"]
+percentiles["Desempleo"] = 1-percentiles["Desempleo"]
+percentiles["Seguro Cesantía"] = 1-percentiles["Seguro Cesantía"]
+percentiles["High Yield Spread"] = 1-percentiles["High Yield Spread"]
+percentiles["Confianza"] = 1-percentiles["Confianza"]
+aphades_macro = pd.DataFrame(percentiles.mean(axis=1), columns=["percentil"])
+
+from scipy import stats
+aphades_macro = pd.DataFrame(stats.zscore(aphades_macro), columns=["modelo"], index=percentiles.index)
+aphades_macro = aphades_macro.rank(pct=True)
+
+#Gráfico
+ax = aphades_macro.plot()
+plt.axvspan("1990-7-1", "1991-3-1", alpha=0.3, color='red') #S&L
+plt.axvspan("2001-3-1", "2001-11-1", alpha=0.3, color='red') #Dotcom
+plt.axvspan("2007-12-1", "2009-5-1", alpha=0.3, color='red') #Subprime (aproximación fechas basados en FRED)
+plt.axhline(0.20, color='black', linewidth=1, linestyle="--")
+plt.axhline(0.90, color='black', linewidth=1, linestyle="--")
+plt.title("US Macro")
+ax1 = ax.twinx()
+sp['^GSPC'].plot.area(ax=ax1, color='b', alpha=0.3)
